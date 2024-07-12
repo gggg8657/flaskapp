7486479e9683590ab7f90332ed6fabfe4735b5dc
DongJu Kim <gggg8657@gmail.com>
2024-07-12 09:41:58 +0900

    setup.sh update

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 # Add the git-core PPA and install git
 sudo add-apt-repository ppa:git-core/ppa -y
 sudo apt-get update
+sudo apt-get install python3-pip -y
 sudo apt-get install git -y
 
 # Install Python packages from requirements.txt
