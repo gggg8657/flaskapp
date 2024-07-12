8c22f17eab9aff532f372009d2a8c79d204d7554
DongJu Kim <gggg8657@gmail.com>
2024-07-12 09:36:31 +0900

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Add the git-core PPA and install git
+sudo add-apt-repository ppa:git-core/ppa -y
+sudo apt-get update
+sudo apt-get install git -y
+
+# Install Python packages from requirements.txt
+pip install -r requirements.txt
